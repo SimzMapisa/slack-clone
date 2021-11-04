cf7ab5acd44c0b2f08c5550a8b4366a319fb5140
Simba Mapisa <simbarashe795@gmail.com>
2021-11-04 06:15:12 +0200

    Initial commit

---
+++ src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyA9kIrd3lMu-wWJC4fEbUisk699hi9Q-UQ",
+    authDomain: "slack-clone-836f2.firebaseapp.com",
+    projectId: "slack-clone-836f2",
+    storageBucket: "slack-clone-836f2.appspot.com",
+    messagingSenderId: "738614787631",
+    appId: "1:738614787631:web:a7974802be1ee3122390da"
+  }; 
